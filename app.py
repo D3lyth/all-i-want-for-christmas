efc4cb68b957cae7e200a6cf16b65d1eba13c76d
D3lyth <>
2023-09-24 21:37:29 +0000

    Add route for '/register' with GET and POST support

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def get_gift_lists():
     gift_lists = mongo.db.gift_lists.find()
     return render_template("giftlists.html", gift_lists=gift_lists)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
